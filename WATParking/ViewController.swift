06d6f4f786f17002bae2fd55a710d86fe6a6d8cd
Yutong Luo <yutong@yutongluo.com>
2015-12-25 11:13:14 -0500

    Initial Commit

---
+++ WATParking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WATParking
+//
+//  Created by Yutong Luo on 12/25/15.
+//  Copyright © 2015 YTL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
